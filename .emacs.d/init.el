26a652d7115b472a391765d8972abf573a06a5c2
Ignacio Vargas Cordero <igvacor94@gmail.com>
2017-09-24 18:23:05 -0500

    added evil-org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,5 +17,23 @@
 (eval-when-compile
   (require 'use-package))
 
-(use-package markdown-mode
-  :ensure t)
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil-org use-package monitor evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
