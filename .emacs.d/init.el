aeb71b78a8709f15b5b53c93765f7132f2a42b95
Ignacio Vargas Cordero <igvacor94@gmail.com>
2017-09-25 20:56:10 -0500

    basic functionality for evil and org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,10 +18,6 @@
 
 (setq package-enable-at-startup nil)
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
-(add-to-list 'exec-path "/usr/local/bin")
-
 ;; Don't litter my init file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
@@ -33,59 +29,119 @@
 (eval-when-compile
   (require 'use-package))
 
-;; Essential settings.
+;; Basic settings
 (setq inhibit-splash-screen t
       inhibit-startup-message t
       inhibit-startup-echo-area-message t)
-(show-paren-mode 1)
-(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
-(setq-default left-fringe-width nil)
-(setq-default indicate-empty-lines t)
-(setq-default indent-tabs-mode nil)
-(setq vc-follow-symlinks t)
-(setq large-file-warning-threshold nil)
-(setq split-width-threshold nil)
-(setq custom-safe-themes t)
-(column-number-mode t)
-(setq tab-width 4)
-
-;; Allow confusing functions
-(put 'narrow-to-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-
-(defun air--delete-trailing-whitespace-in-proc-and-org-files ()
-  "Delete trailing whitespace if the buffer is in `prog-' or `org-mode'."
-  (if (or (derived-mode-p 'prog-mode)
-          (derived-mode-p 'org-mode))
-      (delete-trailing-whitespace)))
-(add-to-list 'write-file-functions 'air--delete-trailing-whitespace-in-proc-and-org-files)
 
 ;; Backups
 (defvar backup-dir "~/.emacs.d/backups/")
 (setq backup-directory-alist (list (cons "." backup-dir)))
 (setq make-backup-files nil)
 
+;; Evil 
 (use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+
+  (use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode))
+
+  (use-package evil-jumper
+    :ensure t
+    :config
+    (global-evil-jumper-mode))
+
+  (use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode))
+
+  (use-package evil-indent-textobject
+    :ensure t))
+
+(dolist (mode '(ag-mode
+                flycheck-error-list-mode
+                git-rebase-mode))
+  (add-to-list 'evil-emacs-state-modes mode))
+
+(add-hook 'occur-mode-hook
+          (lambda ()
+            (evil-add-hjkl-bindings occur-mode-map 'emacs
+              (kbd "/")       'evil-search-forward
+              (kbd "n")       'evil-search-next
+              (kbd "N")       'evil-search-previous
+              (kbd "C-d")     'evil-scroll-down
+              (kbd "C-u")     'evil-scroll-up
+              (kbd "C-w C-w") 'other-window)))
+
+;; Org
+
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
+
+(setq org-agenda-files '("~/Documents/org/"))
+
+(defun air-pop-to-org-agenda (split)
+  "Visit the org agenda, in the current window or a SPLIT."
+  (interactive "P")
+  (org-agenda-list)
+  (when (not split)
+    (delete-other-windows)))
+(define-key global-map (kbd "C-c t a") 'air-pop-to-org-agenda)
+
+(setq org-capture-templates
+      '(("a" "My TODO task format." entry
+         (file "todo.org")
+         "* TODO %?
+SCHEDULED: %t")))
+
+(defun air-org-task-capture ()
+  "Capture a task with my default template."
+  (interactive)
+  (org-capture nil "a"))
+(define-key global-map (kbd "C-c c") 'air-org-task-capture)
+
+(setq org-agenda-text-search-extra-files '(agenda-archives))
+(setq org-blank-before-new-entry (quote ((heading) (plain-list-item))))
+(setq org-enforce-todo-dependencies t)
+(setq org-log-done (quote time))
+(setq org-log-redeadline (quote time))
+(setq org-log-reschedule (quote time))
+
+(defun air-org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+
+(defun air-org-skip-subtree-if-habit ()
+  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (string= (org-entry-get nil "STYLE") "habit")
+        subtree-end
+      nil)))
+
+;; Tiny Menu
+(use-package tiny-menu
   :ensure t)
 
-(require 'evil)
-(evil-mode t)
-
-(use-package evil-org
-  :ensure t
-  :after org
-  :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-            (lambda ()
-              (evil-org-set-key-theme))))
-
+;; Helm
 (use-package helm
   :ensure t)
 
+;; Markdown-Mode
 (use-package markdown-mode
   :ensure t)
 
+;; Dictionary
 (use-package dictionary :ensure t)
 
 (provide 'init)
