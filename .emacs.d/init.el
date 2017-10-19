916c34622e0b6419e860caed6afc029f06115185
Ignacio Vargas Cordero <igvacor94@gmail.com>
2017-10-19 01:09:52 -0500

    simplified init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 
 ;;; Commentary:
 ;;
-;; Largely inspired (stolen) from aaronbieber's init.el
+;; Inspired by aaronbieber's init.el
 ;; https://github.com/aaronbieber/dotfiles/
 ;;
 ;;; Code:
@@ -39,7 +39,7 @@
 (setq backup-directory-alist (list (cons "." backup-dir)))
 (setq make-backup-files nil)
 
-;; Evil 
+;; Evil
 (use-package evil
   :ensure t
   :config
@@ -88,50 +88,6 @@
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
 
 (setq org-agenda-files '("~/Dropbox/org/"))
-
-(defun air-pop-to-org-agenda (split)
-  "Visit the org agenda, in the current window or a SPLIT."
-  (interactive "P")
-  (org-agenda-list)
-  (when (not split)
-    (delete-other-windows)))
-(define-key global-map (kbd "C-c t a") 'air-pop-to-org-agenda)
-
-(setq org-capture-templates
-      '(("a" "My TODO task format." entry
-         (file "todo.org")
-         "* TODO %?
-SCHEDULED: %t")))
-
-(defun air-org-task-capture ()
-  "Capture a task with my default template."
-  (interactive)
-  (org-capture nil "a"))
-(define-key global-map (kbd "C-c c") 'air-org-task-capture)
-
-(setq org-agenda-text-search-extra-files '(agenda-archives))
-(setq org-blank-before-new-entry (quote ((heading) (plain-list-item))))
-(setq org-enforce-todo-dependencies t)
-(setq org-log-done (quote time))
-(setq org-log-redeadline (quote time))
-(setq org-log-reschedule (quote time))
-
-(defun air-org-skip-subtree-if-priority (priority)
-  "Skip an agenda subtree if it has a priority of PRIORITY.
-PRIORITY may be one of the characters ?A, ?B, or ?C."
-  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
-        (pri-value (* 1000 (- org-lowest-priority priority)))
-        (pri-current (org-get-priority (thing-at-point 'line t))))
-    (if (= pri-value pri-current)
-        subtree-end
-      nil)))
-
-(defun air-org-skip-subtree-if-habit ()
-  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
-  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
-    (if (string= (org-entry-get nil "STYLE") "habit")
-        subtree-end
-      nil)))
 
 ;; Tiny Menu
 (use-package tiny-menu
